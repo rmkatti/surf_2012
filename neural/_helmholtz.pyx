7dc70fd1ec94eeff82278341c0e9d11a09c0177a
epatters <ejpatters@gmail.com>
2012-07-03 16:48:02 -0700

    Replace remaining np.appends in wake-sleep cycle with pre-allocated arrays.

--- neural/_helmholtz.pyx
+++ neural/_helmholtz.pyx
@@ -9,34 +9,40 @@ from util import sigmoid
 
 def wake(world, G, G_bias, R, epsilon):
     # Sample data from the world.
-    s = np.append(world(), 1)
+    s = np.ones(R[0].shape[1])
+    s[:-1] = world()
     samples = [ s ]
 
     # Pass sensory data upwards through the recognition network.
     for R_weights in R:
-        s = np.append(sample_indicator(sigmoid(np.dot(R_weights, s))), 1)
+        out = np.ones(R_weights.shape[0] + 1)
+        out[:-1] = sample_indicator(sigmoid(tokyo.dgemv(R_weights, s)))
+        s = out
         samples.insert(0, s)
         
     # Pass back down through the generation network, adjusting weights as we go.
     G_bias += epsilon[0] * (samples[0][:-1] - sigmoid(G_bias))
     for G_weights, inputs, target, step \
             in zip(G, samples, samples[1:], epsilon[1:]):
-        generated = sigmoid(np.dot(G_weights, inputs))
+        generated = sigmoid(tokyo.dgemv(G_weights, inputs))
         tokyo.dger4(step, target[:-1] - generated, inputs, G_weights)
 
 
 def sleep(G, G_bias, R, epsilon):
     # Begin dreaming!
-    d = np.append(sample_indicator(sigmoid(G_bias)), 1)
+    d = np.ones(G_bias.size + 1)
+    d[:-1] = sample_indicator(sigmoid(G_bias))
     dreams = [ d ]
 
     # Pass dream data down through the generation network.
     for G_weights in G:
-        d = np.append(sample_indicator(sigmoid(np.dot(G_weights, d))), 1)
+        out = np.ones(G_weights.shape[0] + 1)
+        out[:-1] = sample_indicator(sigmoid(tokyo.dgemv(G_weights, d)))
+        d = out
         dreams.insert(0, d)
 
     # Pass back up through the recognition network, adjusting weights as we go.
     for R_weights, inputs, target, step \
             in zip(R, dreams, dreams[1:], epsilon[::-1]):
-        recognized = sigmoid(np.dot(R_weights, inputs))
+        recognized = sigmoid(tokyo.dgemv(R_weights, inputs))
         tokyo.dger4(step, target[:-1] - recognized, inputs, R_weights)
