989575fec2157ba720c8266332f258cbce3a21b3
epatters <ejpatters@gmail.com>
2012-07-02 22:14:36 -0700

    Update setup.py for Cython extensions.

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
-from setuptools import setup
+from setuptools import setup, Extension
+from Cython.Distutils import build_ext
 
 setup(
-    name='neural',
-    version='0.1',
+    name = 'neural',
+    version = '0.1',
+    cmdclass = {'build_ext': build_ext},
+    ext_modules = [
+        Extension('neural._util', ['neural/_util.pyx']),
+    ],
 )
