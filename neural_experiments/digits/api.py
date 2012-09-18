f6cb2d7e737176e6e20653dfeef4e15c4fc43190
epatters <ejpatters@gmail.com>
2012-09-19 00:34:06 +0400

    Add digit specificity runner to neural experiments API.

--- neural_experiments/digits/api.py
+++ neural_experiments/digits/api.py
@@ -1,3 +1,4 @@
 from grid_search import GridSearchRunner
 from supervised import SupervisedDigitsRunner
 from unsupervised import UnsupervisedDigitsRunner
+from specificity import DigitSpecificityRunner
