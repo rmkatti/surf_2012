e9de11e4908102bba82296431f3029103cf6dab4
epatters <ejpatters@gmail.com>
2012-09-19 19:10:52 -0700

    Saved PDF plots now have the correct size.

--- neural/ui/save_plot.py
+++ neural/ui/save_plot.py
@@ -9,6 +9,8 @@ def save_plot(plot, filename, size=None):
     """
     if size is None:
         size = plot.outer_bounds
+    else:
+        raise NotImplementedError('Custom sizes not implemented')
 
     if filename.endswith('.pdf'):
         save_plot_pdf(plot, filename, size)
@@ -18,20 +20,32 @@ def save_plot(plot, filename, size=None):
         save_plot_img(plot, filename, size)
 
 
-def save_plot_img(plot, filename, size, dpi=72):
+def save_plot_img(component, filename, size, dpi=72):
     gc = PlotGraphicsContext(size, dpi=dpi)
-    gc.render_component(plot)
+    gc.render_component(component)
     gc.save(filename)
         
-def save_plot_pdf(plot, filename, size):
-    from chaco.pdf_graphics_context import PdfPlotGraphicsContext
-    gc = PdfPlotGraphicsContext(filename=filename,
-                                dest_box=(0.5, 0.5, 5.0, 5.0))
-    gc.render_component(plot)
+def save_plot_pdf(component, filename, size, dpi=72):
+    from kiva.pdf import GraphicsContext
+    from reportlab.pdfgen.canvas import Canvas
+    from reportlab.lib.units import inch
+
+    width = (size[0] / float(dpi)) * inch
+    height = (size[1] / float(dpi)) * inch
+    pagesize = (width, height)
+    canvas = Canvas(filename=filename, pagesize=pagesize)
+
+    gc = GraphicsContext(canvas)
+    old_backbuffer = component.use_backbuffer
+    try:
+        component.use_backbuffer = False
+        component.draw(gc)
+    finally:
+        component.use_backbuffer = old_backbuffer
     gc.save()
 
-def save_plot_svg(plot, filename, size):
+def save_plot_svg(component, filename, size):
     from chaco.svg_graphics_context import SVGGraphicsContext
     gc = SVGGraphicsContext(size)
-    gc.render_component(plot)
+    gc.render_component(component)
     gc.save(filename)
